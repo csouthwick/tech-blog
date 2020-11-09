a38a989995e250dc84bb675ed86544bf396551a3
Chris Southwick <csouthwick@live.com>
2020-11-09 04:28:48 -0500

    dashboard to add, edit, and delete posts

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const sequelize = require('../config/connection');
+const { Post, User } = require('../models');
+
+router.get('/', withAuth, (req, res) => {
+  Post.findAll({
+    where: { user_id: req.session.user_id },
+    attributes: ['id', 'title', 'created_at']
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/new', withAuth, (req, res) => {
+  res.render('new-post', { loggedIn: true });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id },
+    attributes: ['id', 'post_content', 'title', 'created_at']
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render('edit-post', { post, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
