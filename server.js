8589368f62d8b45cc1203fc3842323540a6ba173
Chris Southwick <csouthwick@live.com>
2020-11-08 19:51:00 -0500

    initial server code

---
+++ server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ db: sequelize })
+};
+
+const hbs = exphbs.create({ });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
